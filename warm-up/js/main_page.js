59ceb97e706a061ebbcbabc5f5d6848ea673f44a
brilliantyy <yujia6366@163.com>
2016-03-13 14:46:55 +0800

    Update

--- warm-up/js/main_page.js
+++ warm-up/js/main_page.js
@@ -1,5 +1,9 @@
-(function() {
+(function($) {
 
-
-}());
+    $('div.tag-container span').hover(function() {
+        $(this).css('letterSpacing', '9px');
+    }, function() {
+        $(this).css('letterSpacing', '5px');
+    })
+}(jQuery));
 
